46bb5f8a93cc50c094079aa0aab8120e3e87c27a
mkoohafkan <michael.koohafkan@gmail.com>
2016-11-23 14:17:37 -0800

    updated test file

--- vignettes/test.r
+++ vignettes/test.r
@@ -1,34 +1,21 @@
-library(stringr)
-library(dplyr)
-library(h5)
-
-source("C:/repository/testRAS/R/main.r")
+library(RAStestR)
 
 folder = "C:/PROJECTS/FARGO/NEW - HWF-WF Sediment 2015_clip"           
 
-files = list(
-  quasi = "HWF-WF_2015.p02.hdf",
-  #u30s = "HWF-WF_2015.p07.hdf",
-  u1m = "HWF-WF_2015.p06.hdf",
-  u5m = "HWF-WF_2015.p05.hdf"
-)
 
-isunsteady = list(
-  quasi = FALSE,
-  #u30s = TRUE,
-  u1m = TRUE,
-  u5m = TRUE
-)
+quasi = file.path(folder, "HWF-WF_2015.p08.hdf")
+u30s = file.path(folder, "HWF-WF_2015.p07.hdf")
+u1m = file.path(folder, "HWF-WF_2015.p06.hdf")
+u5m = file.path(folder, "HWF-WF_2015.p05.hdf")
 
-resenvs = replicate(length(files), new.env()) %>% setNames(names(files))
+generate_report(quasi, u30s, "quasi", "unsteady", "quasi-unsteady",
+  "unsteady (dt = 30s)", c("Water Surface", "Invert Elevation", 
+    "Long. Cum Mass Change"), c("", 1:7), 4777, output.type = "html")
 
-for (n in names(files)) {
-  with(resenvs[[n]], {
-    f = h5file(file.path(folder, files[[n]]))
-    wse = readwse(f, isunsteady[[n]])
-    cmi = readcmi(f, isunsteady[[n]], which.rows = 4778)
-    lcmc = readlcmc(f, isunsteady[[n]], which.rows = 4778)
-    h5close(f)
+generate_report(u30s, u1m, "unsteady", "unsteady", "unsteady (dt = 30s)", 
+  "unsteady (dt = 1m)", c("Water Surface", "Invert Elevation",
+    "Long. Cum Mass Change"), c("", 1:7), 4777, output.type = "pdf")
 
-  })
-}
+generate_report(u30s, u5m, "unsteady", "unsteady", "unsteady (dt = 30s)",
+  "unsteady (dt = 5m)", c("Water Surface", "Invert Elevation",
+    "Long. Cum Mass Change"), c("", 1:7), 4777, output.type = "pdf")
