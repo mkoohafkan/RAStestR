ba730f50b9b77736ca8f3e8a0f61e3324d25e9a6
mkoohafkan <michael.koohafkan@gmail.com>
2017-03-07 09:37:02 -0800

    updated `refomat_fields`
    now accepts non-standard field names

--- R/export.r
+++ R/export.r
@@ -1,13 +1,13 @@
 #'Reformat As Long Table
 #'
-#' Reformat RAS data from wide table format (stations are individual 
-#' columns) to long table format (stations and data are collapsed to 
+#' Reformat RAS data from wide table format (stations are individual
+#' columns) to long table format (stations and data are collapsed to
 #' key-value column format.
 #'
 #' @param d The data table, i.e. output from \code{read_table} or
 #'   \code{read_sediment}.
 #' @param data.col The name of the new column holding data values.
-#' @param station.col The name of the new column holding station IDs. 
+#' @param station.col The name of the new column holding station IDs.
 #'   The prefix "XS_" will be stripped from the station IDs.
 #' @return The original data table in long format.
 #'
@@ -22,7 +22,7 @@ to_longtable = function(d, data.col, station.col = "Station") {
 
 #'Reformat As Wide Table
 #'
-#' Reformat RAS data from long table format (i.e. output of 
+#' Reformat RAS data from long table format (i.e. output of
 #' \code{to_longtable}) to wide table format (key values are mapped to
 #' individual columns).
 #'
@@ -43,17 +43,17 @@ to_widetable = function(d, key.col, value.col, key.prefix) {
 
 #' Combine Data Tables
 #'
-#' Combine data tables into a single table. The tables must have 
+#' Combine data tables into a single table. The tables must have
 #'   identical column names. For Data sourced from plans with different
 #'   geometry or stations, use \code{to_longtable} to format the tables
-#'   consistently before combining. If combining tables of different 
+#'   consistently before combining. If combining tables of different
 #'   data, provide explicit names to the tables to easily differentiate
-#'   between variables. 
+#'   between variables.
 #'
 #' @param ... Series of named data sets to combine.
-#' @param data.list Alternative input to \code{...}. A single list of 
+#' @param data.list Alternative input to \code{...}. A single list of
 #'  data tables to combine. If not named, the elements will be named
-#'  sequentially using \code{id.col}, e.g. "table 1", "table 2", 
+#'  sequentially using \code{id.col}, e.g. "table 1", "table 2",
 #'  etc.
 #' @param id.col The name of the new column containing data source IDs.
 #' @return A single data table with an additional column specifying
@@ -73,11 +73,11 @@ combine_data = function(..., data.list, id.col = "table") {
 
 #' Write Data To Clipboard
 #'
-#' Write RAS data to clipboard in comma-seperated format for pasting 
+#' Write RAS data to clipboard in comma-seperated format for pasting
 #' into e.g. Microsoft Excel.
 #'
 #' @param d The data table.
-#' @return Writes the data table into the clipboard in tab-seperated 
+#' @return Writes the data table into the clipboard in tab-seperated
 #'   format.
 #'
 #' @importFrom utils write.table
@@ -95,13 +95,13 @@ data_to_clipboard = function(d) {
 #'
 #' Convert station values to distance upstream.
 #'
-#' @param ld The long-format data, i.e. output from 
+#' @param ld The long-format data, i.e. output from
 #'   \code{to_longtable}.
 #' @param distance.col The new column to hold distance values.
 #' @param station.col The column containing station values.
-#' @param metric If \code{TRUE}, stations are assumed to be in 
-#'   kilometers and distances are returned in meters. If \code{FALSE}, 
-#'   stations are assumed to be in miles and distances are returned in 
+#' @param metric If \code{TRUE}, stations are assumed to be in
+#'   kilometers and distances are returned in meters. If \code{FALSE},
+#'   stations are assumed to be in miles and distances are returned in
 #'   feet.
 #' @return The data table with an additional column of distances.
 #'
@@ -122,19 +122,31 @@ station_to_distance = function(ld, distance.col,
 #' Reformat standard RAStestR fields as standard R data types.
 #'
 #' @param ld A data table.
-#' @param fields A list of fields to reformat.
+#' @param fields The fields to reformat. For non-default Station and Time
+#'   columns, \code{fields} must be a named list identifying the actual names or
+#'   positions of the "Time" and "Station" columns.
 #' @return The data table with reformatted fields.
 #'
 #' @details If the "Time" field is specified, values in that column
 #'   will be formatted as R timestamps (POSIXct). If the "Station"
 #'   field is specified, stations will be converted to numeric values
 #'   (only applicable to long format tables).
-#' 
+#'
+#' @import stringr
 #' @export
-reformat_fields = function(ld, fields = c("Time", "Station")) {
-  if ("Time" %in% fields)
-    ld["Time"] = as.POSIXct(ld$Time, tz = "UTC", format = "%d%b%Y %H:%M:%S")
-  if ("Station" %in% fields)
-    ld["Station"] = as.numeric(gsub("XS_", "", as.character(ld$Station)))
+reformat_fields = function(ld, fields = list("Time", "Station")) {
+  fields = as.list(fields)
+  if (is.null(names(fields)))
+    names(fields) = fields
+  if (!all(names(fields) %in% c("Time", "Station")))
+    stop("Argument 'fields' not recognized")
+  time.col = fields[["Time"]]
+  station.col = fields[["Station"]]
+  if ("Time" %in% names(fields))
+    ld[time.col] = as.POSIXct(ld[[time.col]], tz = "UTC",
+      format = "%d%b%Y %H:%M:%S")
+  if ("Station" %in% names(fields))
+    ld[station.col] = ld[[station.col]] %>% as.character() %>%
+      str_replace("XS_", "") %>% str_replace("[*]", "") %>% as.numeric()
   ld
 }
